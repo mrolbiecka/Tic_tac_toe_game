14754d27430575d73c71e45aac7d3a039a087728
Marta <martarolbiecka02@gmail.com>
2025-04-05 20:32:28 +0200

    Validation of input added

--- main.py
+++ main.py
@@ -8,52 +8,57 @@ CIRCLE = 'o'
 board = [[EMPTY_FIELD for _ in range(COLUMN_NUMBER)] for _ in range(ROW_NUMBER)]
 
 
-def print_board():
+def print_board() -> None:
     for i in board:
         print(f"|{i[0]}| |{i[1]}| |{i[2]}|")
 
 
-def choose_start_player():
+def choose_start_player() -> str:
     current_player = random.choice([CIRCLE, CROSS])
     print(f"Player {current_player} can start the game")
     return current_player
 
 
-def choose_field():
-    row = int(input("Please indicate the number of row (from 1 to 3): " ))
-    column = int(input("Please indicate the number of column (from 1 to 3): " ))
-    while board[row - 1][column - 1] != EMPTY_FIELD:
-        print('Sorry, this field is occupied, please choose different field')
-        row = int(input("Please indicate the number of row (from 1 to 3): "))
-        column = int(input("Please indicate the number of column (from 1 to 3): "))
+def get_validated_input() -> tuple:
+    row, column = None, None
+    while True:
+        numbers = input("Please indicate number of row and column with no spaces (from 1 to 3), example: 3 1:  ").split()
+        if len(numbers) != 2:
+            print("Please enter two numbers separated by space")
+            continue
+        try:
+            row = int(numbers[0])
+            column = int(numbers[1])
+        except ValueError:
+            print("Values must be numbers")
+            continue
+        if not (1 <= row <= 3 and 1 <= column <= 3):
+            print("This number is out of range. Please give correct number")
+            continue
+        if board[row - 1][column - 1] != EMPTY_FIELD:
+            print('Sorry, this field is occupied, please choose different field')
+            continue
+        break
     return row, column
 
 
-def insert_mark(current_player, row, column):
+def insert_mark(current_player, row, column) -> None:
     board[row - 1][column - 1] = CROSS if current_player == CROSS else CIRCLE
 
 
-def check_if_win():
-    who_won = 0
-    if board[0][0] == board[1][0] == board[2][0] != EMPTY_FIELD:
-        who_won = board[0][0]
-    elif board[0][1] == board[1][1] == board[2][1] != EMPTY_FIELD:
-        who_won = board[0][1]
-    elif board[0][2] == board[1][2] == board[2][2] != EMPTY_FIELD:
-        who_won = board[0][2]
-
-    elif board[0][0] == board[0][1] == board[0][2] != EMPTY_FIELD:
-        who_won = board[0][0]
-    elif board[1][0] == board[1][1] == board[1][2] != EMPTY_FIELD:
-        who_won = board[1][0]
-    elif board[2][0] == board[2][1] == board[2][2] != EMPTY_FIELD:
-        who_won = board[2][0]
-
-    elif board[0][0] == board[1][1] == board[2][2] != EMPTY_FIELD:
-        who_won = board[0][0]
-    elif board[0][2] == board[1][1] == board[2][0] != EMPTY_FIELD:
-        who_won = board[0][2]
-    return who_won
+def check_if_win()-> str | None:
+    for i in range(COLUMN_NUMBER):
+        if board[0][i] == board[1][i] == board[2][i] != EMPTY_FIELD:
+            return board[0][i]
+    for i in range(ROW_NUMBER):
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY_FIELD:
+            return board[i][0]
+    for i in range(3):
+        if board[0][0] == board[1][1] == board[2][2] != EMPTY_FIELD:
+            return board[0][0]
+        elif board[0][2] == board[1][1] == board[2][0] != EMPTY_FIELD:
+            return board[0][2]
+    return None
 
 
 def change_player(current_player):
@@ -65,8 +70,8 @@ def run_game():
     print("Below you see our board: ")
     print_board()
     current_player = choose_start_player()
-    while check_if_win() == 0:
-        row, column = choose_field()
+    while check_if_win() is None:
+        row, column = get_validated_input()
         insert_mark(current_player, row, column)
         print_board()
         current_player = change_player(current_player)
